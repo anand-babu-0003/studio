7c1510ab80b6f41bbf7f0e47df4b87a13624c488
karthk <k4036119@gmail.com>
2025-06-16 17:21:49 +0000

    add light fades in the background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased flex flex-col min-h-screen">
+        <div className="light-orb light-orb-1"></div>
+        <div className="light-orb light-orb-2"></div>
+        
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
