2fcae7947a0e6a3fbd27369d2c66eb614a5af82d
karthk <k4036119@gmail.com>
2025-06-19 09:28:36 +0000

    remove navbar and the footer section completly on the page not found pag

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,4 @@
+
 // src/app/not-found.tsx
 import Link from 'next/link';
 import '../styles/not-found.css'; // Import the custom CSS
@@ -6,25 +7,25 @@ export default function NotFound() {
   return (
     <div className="fullscreen-nf-wrapper"> {/* Fullscreen wrapper */}
       <section className="page_404">
-        <div className="container"> {/* Use original class name */}
-          <div className="row"> {/* Use original class name */}
-            <div className="col-sm-12 "> {/* Use original class name */}
+        <div className="container_nf"> {/* Use custom class name for container */}
+          <div className="row_nf"> {/* Use custom class name for row */}
+            <div className="col_sm_12_nf "> {/* Use custom class name */}
               {/* This div uses multiple classes as per original HTML */}
-              <div className="col-sm-10 col-sm-offset-1 text-center">
+              <div className="col_sm_10_nf col_sm_offset_1_nf text_center_nf">
                 <div className="four_zero_four_bg">
                   {/* The GIF itself often contains "404", this h1 is text on top */}
-                  <h1 className="text-center ">404</h1>
+                  <h1 className="text_center_nf ">404</h1>
                 </div>
                 
                 <div className="contant_box_404">
-                  <h3 className="h2">
+                  <h3 className="h2_nf">
                   Look like you&apos;re lost
                   </h3>
                   
                   <p>the page you are looking for not avaible!</p>
 
                   {/* Prominent "404" text placed here */}
-                  <h1 className="text-center internal-404-text">404</h1>
+                  <h1 className="text_center_nf internal_404_text_nf">404</h1>
                   
                   <Link href="/" className="link_404">
                     Go to Home
